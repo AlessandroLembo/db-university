5b800b0c81c66c29371f81d7373b5292cf586bbc
AlessandroLembo <alessandrolembo2@gmail.com>
2023-02-14 17:26:30 +0100

    task 2 query with group by

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -51,7 +51,9 @@ FROM `students`
 GROUP BY YEAR(enrolment_date);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-
+SELECT `office_address`, COUNT(*) AS 'Numero insegnanti'
+FROM `teachers`
+GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 SELECT `exam_id`, ROUND(AVG(`vote`)) AS 'media voti per ogni appello' 
