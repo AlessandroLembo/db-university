7650b5b1d269642fc193493dae7e85605e60252f
AlessandroLembo <alessandrolembo2@gmail.com>
2023-02-15 16:11:00 +0100

    task 5 join

--- tracce_join.sql
+++ tracce_join.sql
@@ -33,7 +33,14 @@ ON `departments`.`id` = `degrees`.`department_id`
 ORDER BY `students`.`surname`;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
-
+SELECT `degrees`.`name`, `courses`.`name` AS `course-name`, `teachers`.`name` AS `teacher-name`, `teachers`.`surname`
+FROM `degrees`
+JOIN `courses`
+ON `degrees`.`id` = `courses`.`degree_id`
+JOIN `course_teacher`
+ON `courses`.`id` = `course_teacher`.`course_id`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`;
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
